fee79f485258a89df5dc0949701970f2305e2028
jbot <jbot@hellabicycles.com>
2019-02-20 15:44:51 -0800

    Got a couple functions working.

--- build.py
+++ build.py
@@ -29,32 +29,56 @@ def main():
 	# Build 'pages' dictionaries
 	for file in content_file_list:
 		#if file == "home.html":
-			path = 'content/'+file
-			content = open(path).read()
+			input_file_path = 'content/'+file
+			output_file_path = 'docs/'+file
+			content = open(input_file_path).read()
 			lines = content.splitlines()
 			count = 1
-			content_array = []
+			page = {'input_file_path': input_file_path, 'output_file_path': output_file_path,}
+			#content_array = []
+			#print(path)
 			for line in lines:
 				if line.startswith('***'):
 					line = line.strip('*')
 					key = line.split(":", maxsplit=1)[0]
 					value = line.split(": ", maxsplit=1)[1]
-					dictionary_term = {key:value}
-					pages.append(dictionary_term)
+					#dictionary_term = {key:value}
+					page[key] = value
 				# else:
 				# 	content_array.append(line)
-				count += 1
+			pages.append(page)
+			count += 1
 			# print(content_array)
-	docs_path = "file://" + os.path.abspath('docs')
+	# print("51")
 	print(pages)
+	# print("53")
+	def extract_html(file):
+		# print(file)
+		extracted = ''
+		lines = file.splitlines()
+		for line in lines:
+			if line.startswith('***'):
+				print(line)
+			else:
+				extracted = extracted + line
+		return extracted
 
-	# For loop to build site
+	# Build the pages
 	for file in content_file_list:
 	#    print('content/'+ file)
 		file_content = open('content/'+ file).read()
 	#    print(file_content)
-		open('docs/'+file, 'w+').write(template_top+file_content+template_bottom)
+		html_content = extract_html(file_content)
+		#print(html_content)
+		open('docs/'+file, 'w+').write(template_top+html_content+template_bottom)
+	
 	# Optionally open in web browser
-	#	webbrowser.open(docs_path + "/" + file)
+	def open_firefox(pages):
+		docs_path = "file://" + os.path.abspath('') + "/"
+		for page in pages:
+			page_path = docs_path + page['output_file_path']
+			print(page_path)
+			webbrowser.open(page_path)
+	open_firefox(pages)
 if __name__ == "__main__":
     main()
