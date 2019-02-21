a3ccead096c161e3480879731fa57e640bd0f921
jbot <jbot@hellabicycles.com>
2019-02-20 18:09:23 -0800

    got all code in functions and whatevs

--- build.py
+++ build.py
@@ -1,20 +1,22 @@
-def main():
-	# Importing dependencies
-	import os
-	import webbrowser
-	import readline
+# Importing dependencies
+import os
+import webbrowser
+import readline
 
-	# Print current status of /docs.
-	#print("Audit bef HTML remove: " + str(os.listdir("docs")))
+send_output_to_browser = False #Optional. True opens in Firefox after building site.
 
+def main():	
 	# Remove all HTML files in /docs.
-	remove_list = os.listdir("docs")
-	for item in remove_list:
-		if item.endswith(".html"):
-			os.remove("docs/" + item)
-
-	# Print current status of /docs.
-	#print("Audit aft HTML remove: " + str(os.listdir("docs")))
+	def clear_docs():
+		# Print current status of /docs.
+		#print("Audit bef HTML remove: " + str(os.listdir("docs")))
+		remove_list = os.listdir("docs")
+		for item in remove_list:
+			if item.endswith(".html"):
+				os.remove("docs/" + item)
+		# Print current status of /docs.
+		#print("Audit aft HTML remove: " + str(os.listdir("docs")))
+	clear_docs()
 
 	# Paths to template files
 	template_top = open('templates/top.html').read()
@@ -26,9 +28,9 @@ def main():
 	# Declare 'pages' list
 	pages = []
 
-	# Build 'pages' dictionaries
-	for file in content_file_list:
-		#if file == "home.html":
+	# Build 'pages' dictionary
+	def build_pages_array():
+		for file in content_file_list:
 			input_file_path = 'content/'+file
 			output_file_path = 'docs/'+file
 			content = open(input_file_path).read()
@@ -48,37 +50,37 @@ def main():
 				# 	content_array.append(line)
 			pages.append(page)
 			count += 1
-			# print(content_array)
-	# print("51")
-	print(pages)
-	# print("53")
+	build_pages_array()
+
+	# Extract HTML from input files
 	def extract_html(file):
 		# print(file)
 		extracted = ''
 		lines = file.splitlines()
 		for line in lines:
-			if line.startswith('***'):
-				print(line)
-			else:
+			if not line.startswith('***'):
 				extracted = extracted + line
 		return extracted
 
 	# Build the pages
-	for file in content_file_list:
-	#    print('content/'+ file)
-		file_content = open('content/'+ file).read()
-	#    print(file_content)
-		html_content = extract_html(file_content)
-		#print(html_content)
-		open('docs/'+file, 'w+').write(template_top+html_content+template_bottom)
+	def build_pages():
+		for file in content_file_list:
+		#    print('content/'+ file)
+			file_content = open('content/'+ file).read()
+		#    print(file_content)
+			html_content = extract_html(file_content)
+			#print(html_content)
+			open('docs/'+file, 'w+').write(template_top+html_content+template_bottom)
+	build_pages()
 	
-	# Optionally open in web browser
+	# Optionally open output in web browser
 	def open_firefox(pages):
-		docs_path = "file://" + os.path.abspath('') + "/"
-		for page in pages:
-			page_path = docs_path + page['output_file_path']
-			print(page_path)
-			webbrowser.open(page_path)
+		if send_output_to_browser == True:
+			docs_path = "file://" + os.path.abspath('') + "/"
+			for page in pages:
+				page_path = docs_path + page['output_file_path']
+				print(page_path)
+				webbrowser.open(page_path)
 	open_firefox(pages)
 if __name__ == "__main__":
-    main()
+	main()
