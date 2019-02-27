a4c793c32784a8459238473d03cb04662cc46159
jbot <jbot@hellabicycles.com>
2019-02-27 15:42:02 -0800

    Implemented glob. Re-factored & removed repetitious code. Simplfied main() function.

--- build.py
+++ build.py
@@ -2,89 +2,106 @@
 import os
 import webbrowser
 import readline
+import glob
 
-send_output_to_browser = True #Optional. True opens in Firefox after building site.
+#Optional. True opens in Firefox after building site.
+send_output_to_browser = True 
 
-def main():	
-	# Remove all HTML files in /docs.
-	def clear_docs():
-		# Print current status of /docs.
-		#print("Audit bef HTML remove: " + str(os.listdir("docs")))
-		remove_list = os.listdir("docs")
-		for item in remove_list:
-			if item.endswith(".html"):
-				os.remove("docs/" + item)
-		# Print current status of /docs.
-		#print("Audit aft HTML remove: " + str(os.listdir("docs")))
-	clear_docs()
+def directoryStatus(folder, fileNames):
+	print('The current state of the', folder, 'directory is:')
+	if len(fileNames) > 1:
+		print(fileNames)
+	else:
+		print('The', folder, 'directory is empty! :)')
 
-	# Read in the entire template
-	template = open('templates/base.html').read()
+def deleteFileList(files):
+	for file in files:
+		os.remove(file)
 
-	# Paths to content
-	content_file_list = os.listdir("content")
-
-	# Declare 'pages' list
+def buildMetaData(files, inputDir, outputDir):
 	pages = []
-
-	# Build 'pages' dictionary
-	def build_pages_array():
-		for file in content_file_list:
-			input_file_path = 'content/'+file
-			output_file_path = 'docs/'+file
-			content = open(input_file_path).read()
-			lines = content.splitlines()
-			count = 1
-			page = {'input_file_path': input_file_path, 'output_file_path': output_file_path,}
-			#content_array = []
-			#print(path)
-			for line in lines:
-				if line.startswith('***'):
-					line = line.strip('*')
-					key = line.split(":", maxsplit=1)[0]
-					value = line.split(": ", maxsplit=1)[1]
-					#dictionary_term = {key:value}
-					page[key] = value
-				# else:
-				# 	content_array.append(line)
-			pages.append(page)
-			count += 1
-	build_pages_array()
+	for file in files:
+		# print('24', file)
+		outputFile = file.replace(inputDir + '/',outputDir + '/')
+		# print('26', outputFile)
+		content = open(file).read()
+		lines = content.splitlines()
+		count = 1
+		page = {'file': file, 'outputFile': outputFile,}
+		for line in lines:
+			if line.startswith('***'):
+				line = line.strip('*')
+				key = line.split(":", maxsplit=1)[0]
+				value = line.split(": ", maxsplit=1)[1]
+				page[key] = value
+		pages.append(page)
+		count += 1
+	if len(pages) > 1:
+		return pages
+	return 'There is no content!'
 
 	# Extract HTML from input files
-	def extract_html(file):
-		# print(file)
-		extracted = ''
-		lines = file.splitlines()
-		for line in lines:
-			if not line.startswith('***'):
-				extracted = extracted + line
-		return extracted
+def extractHTML(file):
+	# print(file)
+	extracted = ''
+	lines = file.splitlines()
+	for line in lines:
+		if not line.startswith('***'):
+			extracted = extracted + line
+	return extracted
 
-	# Build the pages
-	def build_pages_with_base():
-		for file in content_file_list:
-		#    print('content/'+ file)
-			file_content = open('content/'+ file).read()
-		#    print(file_content)
-			html_content = extract_html(file_content)
-			#print(html_content)
-			
-			#open('docs/'+file, 'w+').write(template_top+html_content+template_bottom)
-			
-			# Use the string replace
-			finished_page = template.replace("{{content}}", html_content)
-			open('docs/'+file, 'w+').write(finished_page)
-	build_pages_with_base()
+# Build the pages
+def buildPages(contentMetaData, templates):
+	template = open(templates[0]).read()
+	for item in contentMetaData:
+		file_content = open(item['file']).read()
+		html_content = extractHTML(file_content)
+		finished_page = template.replace("{{content}}", html_content)
+		open(item['outputFile'], 'w+').write(finished_page)
 
-	# Optionally open output in web browser
-	def open_firefox(pages):
-		if send_output_to_browser == True:
-			docs_path = "file://" + os.path.abspath('') + "/"
-			for page in pages:
-				page_path = docs_path + page['output_file_path']
-				#print(page_path)
-				webbrowser.open(page_path)
-	open_firefox(pages)
+# Optionally open output in web browser
+def open_firefox(contentMetaData):
+	if send_output_to_browser == True:
+		docs_path = "file://" + os.path.abspath('') + "/"
+		print(docs_path)
+		for item in contentMetaData:
+			webbrowser.open(docs_path + item['outputFile'])
+
+def main():
+####################################################
+### Cleansing the output directory ###
+####################################################
+	outputDir = 'docs'
+	outputFiles = glob.glob(outputDir+'/'+'*.html')
+	print('41',outputFiles)
+
+	# Print current status of /docs directory.
+	directoryStatus(outputDir, outputFiles)
+
+	# Remove all HTML files in /docs directory.
+	deleteFileList(outputFiles)
+####################################################
+### Building the Content MetaData ###
+####################################################
+	templateDir = 'templates'
+	templates = glob.glob(templateDir+'/'+'*.template')
+
+	inputDir = 'content'
+	inputFiles = glob.glob(inputDir+'/'+'*.*')
+
+	# print('43', inputFiles)
+	# print('44', templates)
+
+	contentMetaData = buildMetaData(inputFiles, inputDir, outputDir)
+	#print(contentMetaData)
+####################################################
+### Generating the Output ###
+####################################################
+	buildPages(contentMetaData, templates)
+####################################################
+### Opening output with Firefox ###
+####################################################
+	open_firefox(contentMetaData)
+
 if __name__ == "__main__":
 	main()
